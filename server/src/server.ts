cc261fb68e93956a7c8a5ea2cf202bda25b8b0c8
Anton Kablukov <antoha7@gmail.com>
2021-06-07 22:38:05 +0200

    refactor server.js

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,7 @@ import { WALLET } from './config/index';
 import { rabbitMqProducer } from './libs/rabbitmq/rabbitmq.producer';
 import { BlockProcessor } from './libs/wallet/blockProcessor/blockProcessor';
 import { logger } from './utils/logger';
+import { BrokerAsPromised } from 'rascal';
 
 checkNodeVersion();
 validateEnv();
@@ -18,33 +19,34 @@ walletStreamer
   .then(() => logger.info('Wallet streaming started successfully'))
   .catch(error => logger.error(error));
 
+let rabbitMQbroker: BrokerAsPromised;
 rabbitMqProducer
   .getBroker()
-  .then(broker => {
-    process
-      .on('SIGINT', async () => {
-        logger.error('SIGINT');
-        await broker.shutdown().then(() => {
-          process.exit();
-        });
-      })
-      .on('SIGTERM', async () => {
-        logger.error('SIGTERM');
-        await broker.shutdown().then(() => {
-          process.exit();
-        });
-      })
-      .on('unhandledRejection', async (reason, p) => {
-        logger.error(`${reason}, 'Unhandled Rejection at Promise', ${JSON.stringify(p)}`);
-        await broker.shutdown().then(() => {
-          process.exit(-1);
-        });
-      })
-      .on('uncaughtException', async err => {
-        logger.error(`${err}, 'Uncaught Exception thrown'`);
-        await broker.shutdown().then(() => {
-          process.exit(-2);
-        });
-      });
-  })
+  .then(broker => (rabbitMQbroker = broker))
   .catch(error => logger.error(error));
+
+process
+  .on('SIGINT', async () => {
+    logger.error('SIGINT');
+    await rabbitMQbroker.shutdown().then(() => {
+      process.exit();
+    });
+  })
+  .on('SIGTERM', async () => {
+    logger.error('SIGTERM');
+    await rabbitMQbroker.shutdown().then(() => {
+      process.exit();
+    });
+  })
+  .on('unhandledRejection', async (reason, p) => {
+    logger.error(`${reason}, 'Unhandled Rejection at Promise', ${JSON.stringify(p)}`);
+    await rabbitMQbroker.shutdown().then(() => {
+      process.exit(1);
+    });
+  })
+  .on('uncaughtException', async err => {
+    logger.error(`${err}, 'Uncaught Exception thrown'`);
+    await rabbitMQbroker.shutdown().then(() => {
+      process.exit(2);
+    });
+  });
